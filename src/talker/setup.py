c9ae0edecc1c3ed2c37a514305a8b278589ff188
Max <you@example.com>
2022-08-14 17:00:29 -0700

    added comma in setup.py (ros2 foxy tut does this)

--- src/talker/setup.py
+++ src/talker/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = talker.main:main'
+            'talker = talker.main:main',
         ],
     },
 )

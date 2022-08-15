eb3a7def2d2e16bf2c9769f54f8d22647914ab3f
Max <you@example.com>
2022-08-15 10:46:46 -0700

    added subscriber

--- src/listener/setup.py
+++ src/listener/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'talker'
+package_name = 'listener'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = talker.main:main',
+            'listener = listener.main:main'
         ],
     },
 )
